871d9c7e1c20bc6d955fb5945aeec50c2b47abd9
dauryellen <dauryellen@live.com>
2021-03-10 14:52:04 -0300

    buscar extrato bancário do cliente

--- chapter-one/lessons/2-primeiro-projeto-com-nodejs/src/index.js
+++ chapter-one/lessons/2-primeiro-projeto-com-nodejs/src/index.js
@@ -36,4 +36,17 @@ app.post("/account", (request, response) => {
   return response.status(201).send();
 });
 
+// ==> (GET) localhost:3333/statement
+app.get("/statement/:cpf", (request, response) => {
+  const { cpf } = request.params;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: "Customer not found!" });
+  }
+
+  return response.json(customer.statement);
+});
+
 app.listen(3333);
